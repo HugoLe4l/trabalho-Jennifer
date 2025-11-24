3d4711ea94edb5aac480df0160de1011db54134a
Hugo <Hugo.souzaleal1@gmail.com>
2025-11-24 20:18:04 -0300

    Nova função de atualizar dados do paciente, feita por felipe frança.

--- src/models/pacientesModal.js
+++ src/models/pacientesModal.js
@@ -1,8 +1,26 @@
 import { db } from "../config/conexao.js";
 
 
-export default async function createPaciente(data){
+export async function createPaciente(data){
     const { nome_Paciente, cpf, telefone, data_nasc, cidade, bairro, rua, num_casa } = data
     const insert = await db.run('INSERT INTO pacientes (nome_Paciente, cpf, telefone, data_nasc, cidade, bairro, rua, num_casa) VALUES(?, ?, ?, ?, ?, ?, ?, ?)', [nome_Paciente, cpf, telefone, data_nasc, cidade, bairro, rua, num_casa])
     return insert
+}
+
+export async function updatePaciente(id, data){
+
+    const { nome_Paciente, cpf, telefone, data_nasc, cidade, bairro, rua, num_casa } = data
+    // Atualiza todos os campos baseados no ID
+    
+    const update = await db.run("UPDATE pacientes SET nome_Paciente = ?, cpf = ?, telefone = ?, data_nasc = ?, cidade = ?, bairro = ?, rua = ?, num_casa = ? WHERE id_Paciente = ?", [nome_Paciente, cpf, telefone, data_nasc, cidade, bairro, rua, num_casa, id])
+    
+    return update
+}
+
+export async function vericaExisteCPF(cpf) {    
+    const VerificaExisteCPF = await db.all(`SELECT * from pacientes WHERE cpf = ?`, [cpf])    
+    if (VerificaExisteCPF.length > 0){
+            return true
+    }
+    return false
 }
--- src/routes/pacienteRota.js
+++ src/routes/pacienteRota.js
@@ -1,7 +1,8 @@
 import { Router } from 'express'
 import { db } from '../config/conexao.js';
-import createPaciente from '../models/pacientesModal.js';
+import { createPaciente, updatePaciente, vericaExisteCPF } from '../models/pacientesModal.js';
 
+import Paciente from '../classes/Paciente.js';
 const router = Router();
 
 //Cadastra novo paciente caso o cpf não esteja cadastrado
@@ -12,23 +13,23 @@ router.post('/cadastrar', async (req, resp) => {
         resp.status(400).json({ success: true, mensagem: "Campos necessarios não foram enviados." })
     } else {
 
-        const VerificaExisteCPF = await db.all('SELECT * FROM pacientes WHERE cpf = ?', [DadosNovoPaciente.cpf])
 
-        if (VerificaExisteCPF.length > 0) {
-            return resp.status(409).json({ success: true, mensagem: "CPF já cadastrado." })
+        const existeCPF = await vericaExisteCPF(DadosNovoPaciente.cpf)
 
-        } else {
-            try {
-                const cadastra = await createPaciente(DadosNovoPaciente)
-                resp.status(201).json({ success: false, mensagem: "Sucesso ao cadastrar o paciente.", resultado: cadastra })
-            }
-            catch(error) {
-                resp.status(500).json({ success: true, error: error, mensagem: "Houve um error ao tentar cadastrar o paciente." })
-            }
-
+        if (existeCPF) {
+            return resp.status(409).json({ success: false, mensagem: "CPF já cadastrado." })
         }
 
-    }
+        try {
+            const cadastra = await createPaciente(DadosNovoPaciente)
+            resp.status(201).json({ success: false, mensagem: "Sucesso ao cadastrar o paciente.", resultado: cadastra })
+        }
+        catch (error) {
+            resp.status(500).json({ success: true, error: error, mensagem: "Houve um error ao tentar cadastrar o paciente." })
+        }
+
+
+}
 })
 
 //Busca todos caso não forneça cpf
@@ -36,19 +37,52 @@ router.get('/dados', async (req, resp) => {
     //Ex.: /pacientes?cpf=123
     const { cpf } = req.query;
 
-    if(cpf){ 
+    if (cpf) {
         const pegaPorCPF = await db.all('SELECT * FROM pacientes WHERE cpf = ?', [cpf])
 
-        if(pegaPorCPF.length < 1){
-            return resp.status(404).json({ success: false, mensagem: "Nenhum paciente encontrado com esse CPF."})
+        if (pegaPorCPF.length < 1) {
+            return resp.status(404).json({ success: false, mensagem: "Nenhum paciente encontrado com esse CPF." })
         }
-        resp.status(200).json({ success:true, mensagem: 'Sucesso ao pegar dados por cpf ', resultado: pegaPorCPF})
+        resp.status(200).json({ success: true, mensagem: 'Sucesso ao pegar dados por cpf ', resultado: pegaPorCPF })
 
-    }else{
+    } else {
         const pegaTodosPorCPF = await db.all('SELECT * FROM pacientes')
-        resp.json({ success:true, mensagem: 'Sucesso ao pegar todos os pacientes ', resultado: pegaTodosPorCPF})
+        resp.json({ success: true, mensagem: 'Sucesso ao pegar todos os pacientes ', resultado: pegaTodosPorCPF })
     }
 })
 
+
+router.put('/atualizar/:id', async (req, resp) => {
+    const { id } = req.params
+    const { DadosPaciente } = req.body
+
+    // Verifica se mandou algum dado
+    if (!DadosPaciente || Object.values(DadosPaciente).every(v => v === "")) {
+        return resp.status(400).json({ success: false, mensagem: "Dados não enviados para atualização." })
+    }
+
+    const existeCPF = await vericaExisteCPF(DadosPaciente.cpf)
+    if (existeCPF) {
+        return resp.status(409).json({ success: false, mensagem: "CPF já cadastrado." })
+    }
+
+
+    try {
+        const busca = await db.all('SELECT * FROM pacientes WHERE id_Paciente = ?', [id])
+
+        if (busca.length < 1) {
+            return resp.status(404).json({ success: false, mensagem: "Paciente não encontrado." })
+        }
+
+        await updatePaciente(id, DadosPaciente)
+
+        resp.status(200).json({ success: true, mensagem: "Paciente atualizado com sucesso." })
+
+    } catch (error) {
+
+        resp.status(500).json({ success: false, error: error, mensagem: "Erro ao atualizar o paciente." })
+    }
+
+})
 
 export default router
